31e3a79cbdf89a97b4a338db1c6bbc4a88e6a657
siris <nicolau.duransilva@sirisacademic.com>
2024-11-13 12:10:50 +0100

    fix: Create expert-profiler branch

---
+++ reviewer-matcher/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="expert_matcher",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # add necessary dependencies here
+    ],
+    description="A package for assigning expert reviewers to project proposals",
+    author="Pablo Accuosto, Madison Chester, Nicolau Duran-Silva"
+    author_email="pablo.accuosto@sirisacademic.com, madison.chester@sirisacademic.com, nicolau.duransilva@sirisacademic.com",
+)
